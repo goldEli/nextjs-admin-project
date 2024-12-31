8fe54df5b45838fc8384f94f9a8b3a2be054abed
eli <eli107377@weexdev.com>
2024-12-31 17:02:43 +0800

    feat: use link

--- src/components/Layout/Header/HeaderTabs/index.tsx
+++ src/components/Layout/Header/HeaderTabs/index.tsx
@@ -8,7 +8,7 @@ export default function HeaderTabs() {
   const { selectedMenuList, selectedMenuItem, setSelectedMenuItem } =
     useMenuStore();
 
-  const selectedMenuId = selectedMenuItem?.id;
+  const selectedMenuLink = selectedMenuItem?.link;
 
   const handleChange = (event: React.SyntheticEvent, newValue: any) => {
     const currentMenu = selectedMenuList.find((item) => item.id === newValue);
@@ -18,14 +18,14 @@ export default function HeaderTabs() {
   return (
     <Box sx={{ bgcolor: "background.paper", width: "100%" }}>
       <Tabs
-        value={selectedMenuId}
+        value={selectedMenuLink}
         onChange={handleChange}
         variant="scrollable"
         scrollButtons="auto"
         aria-label="scrollable auto tabs example"
       >
         {selectedMenuList.map((item) => (
-          <Tab value={item.id} key={item.id} label={item.title} />
+          <Tab value={item.link} key={item.id} label={item.title} />
         ))}
       </Tabs>
     </Box>
