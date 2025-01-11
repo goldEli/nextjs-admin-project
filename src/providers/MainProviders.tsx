36207717c84870eb11372f2fb852998c0ed9bfb5
eli <eli107377@weexdev.com>
2025-01-11 21:41:30 +0800

    feat: add theme

--- src/providers/MainProviders.tsx
+++ src/providers/MainProviders.tsx
@@ -4,12 +4,17 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "@/i18n/config";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import ReactQueryProvider from "./ReactQueryProvider";
+import SystemThemeProvider from "./SystemThemeProvider";
 
 export function MainProviders({ children }: { children: React.ReactNode }) {
   return (
     <ReactQueryProvider>
       <AppRouterCacheProvider>
-        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+        <I18nextProvider i18n={i18n}>
+          <SystemThemeProvider>
+            {children}
+          </SystemThemeProvider>  
+        </I18nextProvider>
       </AppRouterCacheProvider>
     </ReactQueryProvider>
   );
